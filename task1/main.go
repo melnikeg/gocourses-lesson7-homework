e0e82fa32ca2268b369ea6d5367c289059ef6dd3
Egor <melnikeg@ukr.net>
2022-06-26 15:34:44 +0300

    Added task1/main.go

---
+++ task1/main.go
@@ -0,0 +1,24 @@
+// Прибрати всі дублікати з слайсу int.
+// Приклад даних на вхід: [3, 4, 4, 3, 6, 3]
+// видаляємо 3 по індексу 0
+// видаляємо 4 по індексу 1
+// видаляємо 3 по індексу 3
+// Правильний результат: [3, 4, 6]
+// Якщо вам потрібні змінні чи константи - вони мають бути локальними, в межах функції main.
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{4, 1, 4, -4, 6, 3, 8, 8}
+	var result []int
+
+	allKeys := make(map[int]bool)
+	for _, item := range arr {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			result = append(result, item)
+		}
+	}
+	fmt.Println(result)
+}
